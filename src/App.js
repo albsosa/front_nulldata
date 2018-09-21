1678f44cca0279174e61b35bf0451c4f173368a8
albsosa <inf.albsosa@gmail.com>
2018-09-21 08:28:29 -0500

    tabla e insert funcional

--- src/App.js
+++ src/App.js
@@ -16,17 +16,38 @@ constructor() {
       email:'',
       puesto:'',
       fecha_nac:moment(),
-      skills:[{"nombre":"Responsabilidad","calificacion":5},
-{"nombre":"Honestidad","calificacion":4}],
-      calificacion:'',
-      domicilio:''
-
+      skills:'',
+      calificacion:'1',
+      domicilio:'',
+      error:'',
+      tabla:null
 
     }
 }
+
+componentWillMount(){
+this.getRegistros();
+}
+
+getRegistros= ()=>{
+  axios({
+        method: 'get',
+        url:  'http://127.0.0.1:8000/api/verempleados',
+    }).then(response => {
+          this.setState({
+            tabla: response.data.data
+          })
+          console.log(response.data.data)
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+}
   inputHandler = (event) => {
       this.setState({
-        [event.target.name]: event.target.value
+        [event.target.name]: event.target.value,
+        error:''
       })
 
   }
@@ -38,18 +59,44 @@ constructor() {
 
   guardarEmpleado = (event) => {
     //console.log(this.state);
+        const { name, 
+            email,
+            puesto,
+            fecha_nac,
+            domicilio
+          } = this.state;
 
+ const skills = [{nombre: this.state.skills, calificacion: this.state.calificacion}]
+ const data = {name, 
+            email,
+            puesto,
+            fecha_nac,
+            domicilio, skills}
+
+
+
+  if(name!='' && email!='' && puesto!='' && fecha_nac!='' && domicilio!='' && skills[0].nombre!='' &&  skills[0].calificacion!='')
+  {
     axios({
         method: 'post',
         url:  'http://127.0.0.1:8000/api/register',
-        data: this.state,
-    }).then(response => {
+        data: data,
 
+
+    }).then(response => {
+            this.getRegistros();
            console.log(response);
     })
     .catch(error => {
         console.log(error);
     })
+  }
+  else {
+    this.setState({
+      error: 'Favor de llenar todos los campos.'
+    })
+  }
+    
   }
   render() {
     const { name, 
@@ -58,8 +105,28 @@ constructor() {
             fecha_nac,
             skills,
             calificacion,
-            domicilio
+            domicilio, 
+            error,
+            tabla
           } = this.state;
+
+const errorMsj = error!=''?(
+  <div><h1 className="alert">{error}</h1></div>
+  ):(null);
+
+const bodytable = tabla ? (
+  <tbody>
+
+  {tabla.map(user => {
+        return (<tr>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.puesto}</td>
+                </tr>
+                )})}
+</tbody>
+  ) : null;
+
 
 
     return (
@@ -68,11 +135,11 @@ constructor() {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div>
+        <div className="form">
           <span>Nombre</span>
             <input type="text" value={name} onChange={this.inputHandler} name="name"/>
             <span>E-mail</span>
-            <input type="email" value={email} onChange={this.inputHandler}  name="email"/>
+            <input type="text" value={email} onChange={this.inputHandler}  name="email"/>
             <span>Puesto</span>
             <input type="text" value={puesto} onChange={this.inputHandler}  name="puesto"/>
             <span>Fecha Nacimiento</span>
@@ -91,6 +158,18 @@ constructor() {
             </select>
             <button onClick={this.guardarEmpleado} type="button">Envíar</button>
         </div>
+        {errorMsj}
+
+        <table>
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Email</th>
+            <th>Puesto</th>
+          </tr>
+        </thead>
+          {bodytable}
+        </table>
       </div>
     );
   }
